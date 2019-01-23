1108d30ad203daff703436f32c924c57733b8bf2
Paul Cloeter <pdcloeter@gmail.com>
2019-01-23 11:40:38 -0500

    Progress on Controllers and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def login(user)
+    @current_user = user
+    session_token[:session_token] = user.session_token
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def require_logged_in
+    unless current_user
+      render "api/session"
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   validates :username, presence: true, uniquness: true
   validates :password, length: { minimum: 6, allow_nil: true }
   validates :email, presence: true, uniqueness: true
-  
+
   
 
   after_initialize :ensure_session_token
   attr_reader :password
 
-  def self.generate_token
+  def self.generate_session_token
     SecureRandom.urlsafe_base64
   end
 
@@ -30,13 +30,13 @@ class User < ApplicationRecord
     return BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
-  def reset_token!
-    self.session_token = User.generate_token
+  def reset_session_token!
+    self.session_token = User.generate_session_token
     self.save!
     self.session_token
   end
 
   def ensure_session_token
-    return self.session_token ||= User.generate_token
+    return self.session_token ||= User.generate_sesssion_token
   end
 end
