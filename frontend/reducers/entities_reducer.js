588526d60a272232e3e74867acd742a3b9c096c8
Paul Cloeter <pcloeter@gmail.com>
2019-01-26 22:44:57 -0500

    Complete groupsReducer and Update entities Reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,9 +1,11 @@
 import {combineReducers} from "redux";
 import usersReducer from './users_reducer';
+import groupsReducer from './groups_reducer';
 
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
+  groups: groupsReducer
 });
 
 export default entitiesReducer;
---
+++ frontend/reducers/groups_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_GROUPS, RECEIVE_GROUP, REMOVE_GROUP } from '../actions/group_actions';
+import { merge } from 'lodash';
+
+
+const groupsReducer = (oldState, action) => {
+  Object.freeze(oldState);
+  
+  switch (action.type) {
+    case RECEIVE_GROUPS:
+      return action.groups;
+    case RECEIVE_GROUP:
+      return merge({}, oldState, { [action.group.id]: action.group} );
+    case REMOVE_GROUP:
+      let newState = merge({}. oldState);
+      delete newState[action.groupId];
+      return newState;
+    default:
+      return oldState;
+  }
+};
+
+export default groupsReducer;
