3b3c394d3e45dac18dd2b53ea22c34093498d509
Paul Cloeter <pcloeter@gmail.com>
2019-01-28 14:43:16 -0500

    Debug membership

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,9 +1,9 @@
 class Membership < ActiveRecord
-  validates: group_id, user_id, presence: true
+  validates :group_id, :user_id, presence: true
 
-  belongs_to: member,
+  belongs_to :member,
   foreign_key: :user_id
 
-  belongs_to: group,
+  belongs_to :group,
   foreign_key: :group_id
 end
