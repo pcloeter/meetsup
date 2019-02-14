53acaa1401276c139799cd40a852a5c36ec23afe
Paul Cloeter <pcloeter@gmail.com>
2019-02-14 17:58:19 -0500

    CreateEvent constructor

--- frontend/components/events/create_event.jsx
+++ frontend/components/events/create_event.jsx
@@ -6,8 +6,14 @@ class CreateEvent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
-
+      host_id: props.host_id,
+      group_id: props.group_id,
+      title:"",
+      details:"",
+      date:"",
+      time:"",
+      picture_url:"",
+      address:"",
     }
   }
 }
