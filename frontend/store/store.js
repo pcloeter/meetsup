dc82eb11494a6fef300ba041aeb370ef31df736d
Paul Cloeter <pcloeter@gmail.com>
2019-02-18 18:02:40 -0500

    Remove Logger from console

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,6 +3,14 @@ import rootReducer from "../reducers/root_reducer";
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 const configureStore = (preLoadedState = {}) => {
   return createStore(rootReducer, preLoadedState, applyMiddleware(thunk, logger));
 };
